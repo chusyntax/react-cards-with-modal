89fa4344617b36bbc9bb2bc9b017b610561758ee
chu-young <ttheko101@gmail.com>
2021-07-29 21:07:05 +0200

    added all cards

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,55 @@
-import  Cards from './Cards.js'
+import  Card from './Cards.js'
 
 function Projects() {
     return (
       <>
-     <Cards/>
-     <Cards/>
-     <Cards/>
-     <Cards/>
+    <div className="cardWrapper">
+
+   <Card heading="Wassup"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    <Card  heading="Hello"
+   description="Lorem ipsum dolor sits perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+   <Card  heading="AYooo"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    <Card  heading="A Project"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    </div>
+
+    <div className="cardWrapper">
+
+   <Card heading="Wassup"
+   description=" repudiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    <Card  heading="Hello"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. udiandae sit nam blanditiis a alias nostrum porro itaque omnis amet suscipit facere?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+   <Card  heading="AYooo"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    <Card  heading="A Project"
+   description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus perferendis ad aperiam esse, repudiandae sit nam blanditiis a alias nostrum ?"
+   gitHub="https://github.com/chusyntax"
+   liveSite="https://github.com/chusyntax"
+   />
+    </div>
      </>
     );
   }
